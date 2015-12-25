a02e1a4e60fdb8fb53cd394bb2f0b2a65f908da9
arindampradhan <arindampradhan10@gmail.com>
2015-12-25 21:32:00 +0530

    correct url

--- p/__init__.py
+++ p/__init__.py
@@ -2,5 +2,5 @@ __author__='Arindam Pradhan'
 __author_email__='arindampradhan10@gmail.com'
 __maintainer__='Arindam Pradhan'
 __maintainer_email__='arindampradhan10@gmail.com'
-__url__='https://github.com/arindampradhan/poc'
+__url__='https://github.com/arindampradhan/pok'
 __version__ = "0.1.0"
